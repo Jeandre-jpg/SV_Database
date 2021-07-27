65cea971721a893a3feb3017007f746e985ab7ce
Jeandré De Villiers <190025@virtualwindow.co.za>
2021-07-27 17:29:05 +0200

    Initialise Project in XCode

---
+++ Stardew_Library/Stardew_Library/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stardew_Library
+//
+//  Created by Jeandré De Villiers on 2021/07/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stardew_Library/Stardew_Library/Stardew_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stardew_LibraryApp.swift
+//  Stardew_Library
+//
+//  Created by Jeandré De Villiers on 2021/07/27.
+//
+
+import SwiftUI
+
+@main
+struct Stardew_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
